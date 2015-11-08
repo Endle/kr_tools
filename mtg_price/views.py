9bc3b51cf6096d322c19186e495d7ae257363d0c
Zhenbo Li <litimetal@gmail.com>
2015-11-08 11:33:31 +0800

    Form added

--- mtg_price/views.py
+++ mtg_price/views.py
@@ -3,14 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def template():
-    fin = open("home.html")
-    html = fin.read()
-    fin.close()
-    return html
-
 def home(request):
-    #html = template
-    #return HttpResponse("hello")
-    return render(request, "base.html")
+    hello = "Hello!"
+    content = {}
+    content["hello"] = hello
+    return render(request, "base.html", content)
 
