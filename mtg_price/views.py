d54a979e1d8e3abfbcaf6aa33ad8c1e99998001d
Zhenbo Li <litimetal@gmail.com>
2015-11-08 14:26:54 +0800

    成功获取 form 的数据

--- mtg_price/views.py
+++ mtg_price/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    hello = "Hello!"
-    content = {}
-    content["hello"] = hello
-    return render(request, "base.html", content)
-
+    if request.method == 'GET':
+        content = {}
+        content["result"] = "Hello"
+        getter = request.GET.dict()
+        if "name" in getter:
+            content["result"] = "English name: " + getter['name']
+        if 'name_cn' in getter:
+            content['result'] += " Chinese name: " + getter['name_cn']
+        return render(request, "base.html", content)
