f781cd40c09739f6fdebff7f9cc2866d8a92dee9
Zhenbo Li <litimetal@gmail.com>
2015-11-14 23:39:45 +0800

    封装了 browser

--- mtg_price/mtginfo.py
+++ mtg_price/mtginfo.py
@@ -1,25 +1,16 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
-from selenium import webdriver
 from mtg_price.models import Card_EN, Card_CN
 from django.core.exceptions import ObjectDoesNotExist
+import browser
 import logging
 logger = logging.getLogger(__name__)
 
 def _nameCN_to_nameEN_slow(nameCN:str) -> str:
     url = "http://magiccards.info/query?q=!" + urllib.parse.quote(nameCN) + "&v=card&s=cname"
     logger.warn("Fetching MTG Info: %s" % url)
-    header = {'GET': '',
-                'User-Agent': "Mozilla/5.0 (Windows NT 6.2; rv:29.0) Gecko/20100101 Firefox/29.0",
-                'Referer': "http://magiccards.info/",
-            'Host': "magiccards.info",
-    }
-
-    driver = webdriver.PhantomJS()
-    driver.get(url)
-    code = driver.page_source
-    driver.close()
+    code = browser.fetch(url)
     soup = BeautifulSoup(code, "lxml")
     not_found = soup.find_all(text="没有匹配的结果")
     if len(not_found) > 0:
@@ -42,8 +33,4 @@ def nameCN_to_nameEN(nameCN:str) -> str:
         card_en = Card_EN.objects.create(name=result)
         card_cn = Card_CN.objects.create(en=card_en,cn=nameCN)
     return result
-
-if __name__ == "__main__":
-    result = nameCN_to_nameEN("塔莫耶夫")
-    print(result)
 
