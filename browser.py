880d810a9ef1bd7621694a9e4a3c01f5bc58666e
Zhenbo Li <litimetal@gmail.com>
2015-11-15 20:58:48 +0800

    加入了缓存功能

--- browser.py
+++ browser.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 import threading
+import cachetools
 import logging
 logger = logging.getLogger(__name__)
 
 _driver = webdriver.PhantomJS()
 _locker = threading.Lock()
 
-def fetch(url:str):
+def _fetch(url:str):
     logger.warn("id of webdriver %d" % id(_locker))
     with _locker:
         _driver.get(url)
         code = _driver.page_source
         return code
 
+_cache = cachetools.TTLCache(maxsize=128, ttl=3600, missing=_fetch) # 默认一小时内缓存有效
+
+def fetch(url:str)->str:
+    return _cache[url]
