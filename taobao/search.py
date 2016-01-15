d8370f5f17604ed5cf92dfb8fdf18b5313feeee2
Zhenbo Li <litimetal@gmail.com>
2016-01-15 12:11:16 +0800

    完成了抓取淘宝信息的工作

--- taobao/search.py
+++ taobao/search.py
@@ -24,15 +24,40 @@ def _get_url(name:str)->str:
 
 def _solve_item(bs_item) -> ItemData:
     assert type(bs_item) is element.Tag
+    assert list(bs_item["class"]) == ['item', 'g-clearfix']
     item = ItemData()
-    #col2 = bs_item.find_all("div", class="col-2")
-    #print(col2)
-    print("---------------")
-    #col3 = bs_item.fetch
-    print("=============")
+    col2 = col3 = None
+    for i in bs_item.contents:
+        if type(i) == element.NavigableString: continue
+        if list(i["class"]) == ["col", "col-2"]: col2 = i
+        if list(i["class"]) == ["col", "col-3"]: col3 = i
+    assert col2 != None
+    assert col3 != None
+
+    assert type(col2) is element.Tag
+    a = col2.a
+    assert type(a) is element.Tag
+    assert a["class"] == ["J_ClickStat"]
+    item.link = "http:" + a["href"] # Hacky, 不明白为何需要加前缀
+    item.name = "".join(a.stripped_strings)
+
+    redundancy_div = col3.div
+    price_span = redundancy_div.span
+    assert price_span["class"] == ["price", "g_price", "g_price-highlight"]
+    item.price = float(price_span.strong.string)
+    shipping_div = None
+    for i in col3.contents:
+        if type(i) == element.NavigableString: continue
+        try:
+            if list(i["class"]) == ["ship"]: shipping_div = i
+        except KeyError:
+            continue
+    shipping_price = shipping_div.string.split()[-1]
+    item.shipping_cost = float(shipping_price)
+
     return item
 
-def search(name:str)->str:
+def search(name:str)->[ItemData]:
     url = _get_url(name)
     #code = browser.fetch(url)
     #with open("/dev/shm/page.html", 'w') as fout:
@@ -43,18 +68,15 @@ def search(name:str)->str:
     assert type(item_list) is element.ResultSet
     assert len(item_list) == 1
     redundancy_child = item_list[0].div.contents #<div class="items g-clearfix">
-    with open("/dev/shm/result.html", "w") as fout:
-        fout.write(str(redundancy_child))
+    #with open("/dev/shm/result.html", "w") as fout:
+        #fout.write(str(redundancy_child))
 
     item_list = [] # list of ItemData
     for item in redundancy_child:
         if str(item).strip() == "":
             continue
         item_list.append(_solve_item(item))
-    print(item_list)
     return(str(redundancy_child))
-
-
 
 if __name__ == '__main__':
     ret = search("众神之怒 万智牌")
